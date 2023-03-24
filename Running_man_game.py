608849441ced85d81708679b1e633c1e52368b4f
Beyazıt UYSAL <66223190+masanbasa3k@users.noreply.github.com>
2023-03-24 07:22:40 +0300

    Update Running_man_game.py

--- Running_man_game.py
+++ Running_man_game.py
@@ -4,9 +4,6 @@ import random
 
 pygame.init()
 
-GLB_SPD = 10
-STAT_FONT = pygame.font.SysFont("comicsans", 50)
-
 SCREEN_WIDTH, SCREEN_HEIGHT = 640*2, 480
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Light in the Space")
@@ -31,211 +28,199 @@ CLOUD = [loadImage("spr_clouds1.png"),
 
 BG = pygame.transform.scale((loadImage("spr_bg.png")), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
 class Man:
-    MAX_ROTATION = 25
-    ROT_VEL = 20
-    ANIMATION_TIME = GLB_SPD
-    JUMP_VEL = 10.5
+    X_POS = 32
+    Y_POS = 260
+    JUMP_VEL = 8.5
 
-    def __init__(self,x,y):
+    def __init__(self):
         self.run_img = RUNNING
         self.jump_img = JUMPING
-        
-        self.x = x
-        self.y = y
-        self.tilt = 0
-        self.tick_count = 0
-        self.vel = 0
-        self.height = self.y
-        self.img_count = 0
-        self.img = self.run_img[0]
-        self.jump_vel = self.JUMP_VEL
 
-        self.man_run = True 
+        self.man_run = True
         self.man_jump = False
+        
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.man_rect = self.image.get_rect()
+        self.man_rect.x = self.X_POS
+        self.man_rect.y = self.Y_POS
 
-    def jump(self):
-        self.img = self.jump_img
-        if self.man_jump:
-             self.y -= self.jump_vel * 4
-             self.jump_vel -= 0.4
-        if self.jump_vel < - self.JUMP_VEL:
-            self.jump_vel = self.JUMP_VEL
-            self.man_jump = False
-    
     def update(self, userInput):
         if self.man_run:
-            self.move()
+            self.run()
         if self.man_jump:
             self.jump()
+        
+        if self.step_index >= 10:
+            self.step_index = 0
 
-        if userInput[pygame.K_SPACE] and not self.man_jump:
+        if userInput[pygame.K_SPACE] and not self.man_jump and self.man_rect.y == 260:
             self.man_run = False
             self.man_jump = True
         elif not (self.man_jump):
             self.man_jump = False
             self.man_run = True
+            # self.X_POS == 260
 
-    def move(self):
-        self.tick_count += 1
-        displacement = self.vel*(self.tick_count) + 0.5*(3)*(self.tick_count)**2  # calculate displacement
-        if displacement >= 16:
-            displacement = (displacement/abs(displacement)) * 16
+    def run(self):
+        self.image = self.run_img[self.step_index // 5]
+        self.man_rect = self.image.get_rect()
+        self.man_rect.x = self.X_POS
+        self.man_rect.y = self.Y_POS
+        self.step_index += 1
 
-        if displacement < 0:
-            displacement -= 2
-
-        self.y = self.y + displacement
-
-        if self.y + self.img.get_height() >= 400:
-            self.y  = 400 - self.img.get_height()
+    def jump(self):
+        self.image = self.jump_img
+        if self.man_jump:
+             self.man_rect.y -= self.jump_vel * 4
+             self.jump_vel -= 0.8
+        if self.jump_vel < - self.JUMP_VEL:
+            self.man_jump = False
+            self.jump_vel = self.JUMP_VEL
         
-    def draw(self, win):
-        self.img_count += 1
-        if self.img_count < self.ANIMATION_TIME:
-            self.img = self.run_img[0]
-        elif self.img_count < self.ANIMATION_TIME*2:
-            self.img = self.run_img[1]
-        elif self.img_count < self.ANIMATION_TIME*3:
-            self.img = self.run_img[1]
-        elif self.img_count < self.ANIMATION_TIME*3+1:
-            self.img = self.run_img[0]
-            self.img_count = 0
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.man_rect.x, self.man_rect.y))
 
-        rotated_img = pygame.transform.rotate(self.img, self.tilt)
-        new_rect = rotated_img.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
-        win.blit(rotated_img,new_rect.topleft)
+class Cloud:
+    def __init__(self):
+        self.x = SCREEN_WIDTH + random.randint(800, 1000)
+        self.y = random.randint(50, 100)
+        self.image = CLOUD[random.randint(0, 3)]
+        self.width = self.image.get_width()
+        self.step_index = 0
 
-    def get_mask(self):
-        return pygame.mask.from_surface(self.img)
+    def update(self):
+        self.x -= game_speed
+        if self.x < -self.width:
+            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.y = random.randint(50, 100)
     
-class Base:
-    VEL = GLB_SPD
-    WIDTH = BG.get_width()
-    IMG = BG
-
-    def __init__(self, y):
-        self.y = y
-        self.x1 = 0
-        self.x2 = self.WIDTH
-
-    def move(self):
-        self.x1 -= self.VEL
-        self.x2 -= self.VEL
-
-        if self.x1 + self.WIDTH < 0:
-            self.x1 = self.x2 + self.WIDTH
-
-        if self.x2 + self.WIDTH < 0:
-            self.x2 = self.x1 + self.WIDTH
-
-    def draw(self, win):
-        win.blit(self.IMG, (self.x1, self.y))
-        win.blit(self.IMG, (self.x2, self.y))
-
-def draw_window(win, player, obs, base, score):
-    base.draw(win)
-    for ob in obs:
-        ob.draw(win)
-
-    text = STAT_FONT.render("Score: " + str(score), 1, (85,0,0))
-    win.blit(text, (SCREEN_WIDTH - 10 - text.get_width(), 10))
-
-    
-    player.draw(win)
-    pygame.display.update()
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.x, self.y))
 
 class Obstacle:
-    VEL = GLB_SPD
-    def __init__(self, image, x,y):
-        self.x = x
-        self.y = y
+    def __init__(self, image, type):
         self.image = image
-        self.passed = False
+        self.type = type
+        self.rect = self.image.get_rect()
+        self.rect.x = SCREEN_WIDTH
 
-    def move(self):
-        self.x -= self.VEL
-        
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
     
-    def draw(self, win):
-        win.blit(self.image, (self.x, self.y))
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, self.rect)
 
-    def collide(self, player):
-        player_mask = player.get_mask()
-        mask = pygame.mask.from_surface(self.image)
+class SmallSpikes(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 370
 
-        offset = (self.x - player.x, self.y - round(player.y))
-
-        point = player_mask.overlap(mask, offset)
-
-        if point:
-            return True
-        
-        return False
+class TallSpikes(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = 350
 
 def main():
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles
     run = True
-    
-    win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Man(40,0)
-    base = Base(0)
-    
+    player = Man()
+    cloud = Cloud()
     game_speed = 14
     x_pos_bg = 0
     y_pos_bg = 0
     points = 0
-    obstacles = [Obstacle(SMALL_SPIKES,SCREEN_WIDTH-10,370)]
+    font = pygame.font.Font("freesansbold.ttf", 20)
+    obstacles = []
+    death_count = 0
 
-    score = 0
+    def score():
+        global points, game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed += 1
+        
+        text = font.render(f"Points: {points}", True, (0,0,0))
+        textRect = text.get_rect()
+        textRect.midleft = (40, 50)
+        SCREEN.blit(text, textRect)
+
+    def background():
+        global x_pos_bg,y_pos_bg
+        image_width = BG.get_width()
+        SCREEN.blit(BG, (x_pos_bg,y_pos_bg))
+        SCREEN.blit(BG, (image_width + x_pos_bg,y_pos_bg))
+        if x_pos_bg <= -image_width:
+            SCREEN.blit(BG, (image_width + x_pos_bg,y_pos_bg))
+            x_pos_bg = 0
+        x_pos_bg -= game_speed
 
     while run:
-        
-        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()     
                 quit()
 
-        keys = pygame.key.get_pressed()
-        player.update(keys)  
+        SCREEN.fill((255,255,255))
+        userInput = pygame.key.get_pressed()
 
-        add_obs = False
-        rem = []
-        for obs in obstacles:
-            if obs.collide(player):
-                run = False
-            
-            if obs.x < -64:
-                rem.append(obs)
+        background()
+        player.draw(SCREEN)
+        player.update(userInput)
 
-            if not obs.passed and obs.x < player.x:
-                obs.passed = True
-                add_obs = True
-            obs.move()
+
+        if len(obstacles) == 0:
+            if random.randint(0, 1) == 0:
+                obstacles.append(SmallSpikes(SMALL_SPIKES))
+            elif random.randint(0, 1) == 1:
+                obstacles.append(TallSpikes(TALL_SPIKES))
+
+        for obstacle in obstacles:
+            obstacle.draw(SCREEN)
+            obstacle.update()
+            if player.man_rect.colliderect(obstacle.rect):
+                pygame.time.delay(200)
+                death_count += 1
+                menu(death_count)
         
-        if add_obs:
-            img = SMALL_SPIKES
-            y = 370
-            score += 10
-            if random.randrange(0,2) == 0:
-                img = SMALL_SPIKES
-                y = 370
-            else:
-                img = TALL_SPIKES
-                y = 350
-            obstacles.append(Obstacle(img,SCREEN_WIDTH-10,y))
+
+        cloud.draw(SCREEN)
+        cloud.update()
         
-        for r in rem:
-            obstacles.remove(r)
+        score()
 
-        player.move()
-        base.move()
-        draw_window(win, player, obstacles, base, score)
-    pygame.quit()     
-    quit()
+        clock.tick(30)
+        pygame.display.update()
 
-if __name__ == '__main__':
-    main()
+def menu(death_count):
+    global points
+    run = True
+    while run:
+        SCREEN.fill((255,255,255))
+        font = pygame.font.Font("freesansbold.ttf", 20)
+
+        if death_count == 0:
+            text = font.render("Press any key to start", True, (0,0,0))
+        elif death_count > 0:
+            text = font.render("Press any key to start", True, (0,0,0))
+            score = font.render(f"Your Score {points}", True, (0,0,0))
+            scoreRect = score.get_rect()
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+            SCREEN.blit(score, scoreRect)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        SCREEN.blit(text, textRect)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+            if event.type == pygame.KEYDOWN:
+                main()
+
+menu(death_count=0)
